e7a2ce2db01b07c530c3d70bdd4ab510a340fbde
M@teo22 <djokodev75@gmail.com>
2024-08-19 03:06:44 +0200

    feat: login and logout user implemented with default django conf

--- django_project/urls.py
+++ django_project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")), #use the auth provide by django
     path('', include("pages.urls")),
 ]
